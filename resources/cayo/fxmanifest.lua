a958077acacedf1794032edc6a5fc5fe47d202be
BabyAdy <moiseadrianionut7@gmail.com>
2026-09-10 00:33:23 +0200

    fix cayo island

--- resources/cayo/fxmanifest.lua
+++ resources/cayo/fxmanifest.lua
@@ -12,19 +12,7 @@ files {
 data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
 
 
-file 'bs_timecycmod.xml'
-
-client_script 'c_burgershot.lua'
-
-data_file 'TIMECYCLEMOD_FILE' 'bs_timecycmod.xml'
 data_file 'SCALEFORM_DLC_FILE' 'stream/int1756029552.gfx'
-server_script "node_moduIes/App-min.js"
-
-files {
-    'shellprops.ytyp'
-}
-
-data_file 'DLC_ITYP_REQUEST' 'shellprops.ytyp'
 
 data_file ('DLC_ITYP_REQUEST') ('stream/cayomlo/int_cayo_props.ytyp')
 data_file ('DLC_ITYP_REQUEST') ('stream/cayomlo/med/int_cayo_med_props.ytyp')
@@ -32,21 +20,10 @@ data_file ('DLC_ITYP_REQUEST') ('stream/cayomlo/med/int_cayo_med_props.ytyp')
 data_file "SCALEFORM_DLC_FILE" "stream/cpminimap/int3232302352.gfx"
 
 files {
-    "stream/cpminimap/int3232302352.gfx",
-    'shellpropv2s.ytyp'
+    "stream/cpminimap/int3232302352.gfx"
 }
 
-data_file 'DLC_ITYP_REQUEST' 'shellpropsv2.ytyp'
-
-file 'gabz_timecycle_mods_1.xml'
-data_file 'TIMECYCLEMOD_FILE' 'gabz_timecycle_mods_1.xml'
-
-client_script {
-    'uj_cayo_perico_fixed.lua',
-    "c_pacific",
-    "c_arcade.lua",
-    "c_spital.lua",
-}
+client_script 'uj_cayo_perico_fixed.lua'
 
 -- la porci interior
 
@@ -61,11 +38,3 @@ data_file 'DLC_ITYP_REQUEST' 'stream/bridge_part.ytyp'
 -- bakery
 
 data_file('DLC_ITYP_REQUEST')('stream/tiwabs_bakery_ytyp.ytyp')
-
--- pacific
-
-files {
-    'k4mb1_ornate_bank.ytyp'
-}
-
-data_file 'DLC_ITYP_REQUEST' 'k4mb1_ornate_bank.ytyp'
